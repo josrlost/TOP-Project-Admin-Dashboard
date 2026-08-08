0e5b7e81214283f1f5d04e161ba9abf619eeaff6
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-08-08 03:09:18 -0500

    Added through JS the ability to toggle between themes by adding an event listener to the toggle event button that assigns a dark class to the root pseudo class

--- script/script.js
+++ script/script.js
@@ -19,3 +19,12 @@ btnUpload.addEventListener('mouseup', releasingBtn);
 
 btnShare.addEventListener('mousedown', pressingBtn);
 btnShare.addEventListener('mouseup', releasingBtn);
+
+const root = document.documentElement;
+
+function setTheme() {
+    const newTheme = root.className === 'dark' ? '' : 'dark';
+    root.className = newTheme;
+}
+
+btnToggle.addEventListener('click', setTheme);
