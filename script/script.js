821e1a36103cdf6f0cbff95ea89e6762937d1817
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-08-08 02:53:33 -0500

    Added and link a JavaScript file to the main html file. Added an event listener to the main three buttons within the header section, for a mousedown and mouseup event to change appearence to simulate the pressing effect

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const btnToggle = document.querySelector('.toggleBtn');
+const btnNew = document.querySelector('#btnNew')
+const btnUpload = document.querySelector('#btnUpload');
+const btnShare = document.querySelector('#btnShare');
+
+function pressingBtn (evt) {
+    evt.target.style.backgroundColor = '#ddbeaa';
+}
+
+function releasingBtn (evt) {
+    evt.target.style.backgroundColor = 'var(--backgroundColorAside)';
+}
+
+btnNew.addEventListener('mousedown', pressingBtn);
+btnNew.addEventListener('mouseup', releasingBtn);
+
+btnUpload.addEventListener('mousedown', pressingBtn);
+btnUpload.addEventListener('mouseup', releasingBtn);
+
+btnShare.addEventListener('mousedown', pressingBtn);
+btnShare.addEventListener('mouseup', releasingBtn);
